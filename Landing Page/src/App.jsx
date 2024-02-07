90c0306cdb5e562daf9280efb839297d605c63ab
Katherine Reyes <katherinereyes@MacBook-Pro-de-Katherine.local>
2024-02-07 14:01:58 -0500

    first steps

---
+++ Landing Page/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Navbar } from "./components/Navbar/Navbar"
+import { Body } from "./components/Body/Body"
+export default function App (){
+    return(
+        <>
+        <Navbar/>
+        <Body/>
+        </>
+    )
+}
