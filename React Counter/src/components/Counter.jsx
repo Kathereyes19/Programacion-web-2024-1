81dcd651c25d21f5b66555b7abb6643befa73ca1
Kathereyes19 <kathe2004119@gmail.com>
2024-02-22 17:24:06 -0500

    logical organization to add a road with cars

--- React Counter/src/components/Counter.jsx
+++ React Counter/src/components/Counter.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 import './Counter.css';
+import redcar1 from '../assets/redcar1.png';
+import redcar2 from '../assets/redcar2.png';
+import roadBack from '../assets/roadBack.jpg';
 
 export function Counter() {
   const [count, setCount] = useState(0);
+  const [showIncreaseImage, setShowIncreaseImage] = useState(false);
+  const [showDecreaseImage, setShowDecreaseImage] = useState(false);
 
   const increaseCount = () => {
     setCount(count + 1);
+    setShowIncreaseImage(true);
+    setTimeout(() => setShowIncreaseImage(false), 2000); // Duración fija de 2 segundos
   };
 
   const decreaseCount = () => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
+      setShowDecreaseImage(true);
+      setTimeout(() => setShowDecreaseImage(false), 2000); // Duración fija de 2 segundos
+    }
   };
 
   return (
-    <div>
+    <div className="counter-container">
+      <img src={roadBack} className='background-image' alt="Carretera" />
       <h1>{count}</h1>
       <button onClick={decreaseCount}>Reducir</button>
       <button onClick={increaseCount}>Aumentar</button>
+      {showIncreaseImage && <img src={redcar1} className="moving-image-increase" alt="Carro aumentando" />}
+      {showDecreaseImage && <img src={redcar2} className="moving-image-decrease" alt="Carro disminuyendo" />}
     </div>
   );
 }
