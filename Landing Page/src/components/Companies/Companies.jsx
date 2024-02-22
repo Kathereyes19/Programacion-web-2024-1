1c70515f945597ef929ff04855cd814cd3ce02fb
Kathereyes19 <kathe2004119@gmail.com>
2024-02-22 11:14:32 -0500

    organization of the companies component in the mobile version

--- Landing Page/src/components/Companies/Companies.jsx
+++ Landing Page/src/components/Companies/Companies.jsx
@@ -1,13 +1,19 @@
 import gifImage2 from "../../assets/body2.gif";
+import companies from "../../assets/companies.jpeg"
+
 import './Companies.css';
 
 export function Companies() {
-    return (
-<section className="section-companies">
-  <h2 className="gif-section-title-companies">Empresas de renombre que confían en Canva</h2>
-  <div className="second-gif-container-companies">
-    <img src={gifImage2} alt="GIF" className="gif-image2-companies" />
-  </div>
-</section>
-    )
+  return (
+    <section className="section-companies">
+      <h2 className="gif-section-title-companies">Empresas de renombre que confían en Canva</h2>
+      <div className="second-gif-container-companies">
+        <img src={gifImage2} alt="GIF" className="gif-image2-companies" />
+      </div>
+      <div className="image-companies">
+        <img src={companies} alt="Empresas" />
+      </div>
+    </section>
+  );
 }
+
