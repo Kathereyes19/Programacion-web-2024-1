5056a252de65ab974d88f4bb1e7e896f0e539ea4
Kathereyes19 <kathe2004119@gmail.com>
2024-02-23 11:05:41 -0500

    reset integration with timer

--- React Counter/src/components/Counter/Counter.jsx
+++ React Counter/src/components/Counter/Counter.jsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Counter.css'; 
 import { Buttons } from './../Buttons/Buttons'; 
 import redcar1 from '../../assets/redcar1.png';
 import redcar2 from '../../assets/redcar2.png';
 import roadBack from '../../assets/roadBack.jpg';
+import resetimage from '../../assets/resetimage.webp';
 
 export function Counter() {
   const [count, setCount] = useState(0);
   const [showIncreaseImage, setShowIncreaseImage] = useState(false);
   const [showDecreaseImage, setShowDecreaseImage] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [showResetImage, setShowResetImage] = useState(false);
+
+  const startTimer = () => {
+    setTimer(setTimeout(() => {
+      setGameOver(true);
+      setShowResetImage(true);
+      setTimeout(() => {
+        setCount(0);
+        setGameOver(false);
+        setShowResetImage(false);
+        startTimer();
+      }, 2000); // 2000 milisegundos = 2 segundos
+    }, 60000)); // 60000 milisegundos = 1 minuto
+  };
+
+  const resetGame = () => {
+    setCount(0);
+    setGameOver(false);
+    clearTimeout(timer);
+    startTimer();
+  };
+
+  useEffect (() => {
+    startTimer();
+    return () => clearTimeout(timer);
+  }, []);
 
   const increaseCount = () => {
     setCount(count + 1);
@@ -34,6 +63,7 @@ export function Counter() {
           <Buttons onClick={increaseCount} text="Go running" type="Go" />
           {showIncreaseImage && <img src={redcar1} className="moving-image-increase" />}
           {showDecreaseImage && <img src={redcar2} className="moving-image-decrease" />}
+          {gameOver && <img src={resetimage} className="game-over-image" />}
         </div>
       </div>
     </section>
