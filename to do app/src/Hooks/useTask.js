5b03d145fc4b02b15fe6a3ebf4adc555f6f7b423
Kathereyes19 <kathe2004119@gmail.com>
2024-03-01 11:15:43 -0500

    useTask hook - fixes missing

--- to do app/src/Hooks/useTask.js
+++ to do app/src/Hooks/useTask.js
@@ -34,4 +34,76 @@ export const useTask = () => {
         }
         setTask(prevState => [...prevState, NewTask])
     }
+
+    const HasTask = Task.length > 0 //to know if there are tasks
+
+    const handleToggle = (data) => {
+
+        const { id, completed } = data
+        const NewTask = Task.map (task => {
+            if (task.id === id) {
+                const NewTask = {
+                    ...task,
+                    completed
+                }
+                return NewTask
+            }
+            return task
+        })
+        setTask (NewTask)
+    }
+
+    const handleDelete = (data) => {
+        const { id } = data
+        const DeleteTask = Task.filter((task) => task.id !== id)
+        setTask(DeleteTask) 
+       }
+
+    const handleFilterChange = (filterValue) => {
+        setFilters(filterValue)
+    }  
+    
+    const filteredTask = task.filter((task) => {
+        if (currentFilters === 'completed') {
+            return task.completed
+        } if (currentFilters === 'pending') {
+            return task.completed
+        } else {
+            return task
+        }
+    })
+
+    const completedTask = Task.map(task => task.completed).length
+    const allTask = Task.length
+
+    const handleDeleteAll = () => {
+        const completedTaskIds = Task.filter((task) => task.completed).map((task) => task.id)
+    
+        const updatedTask = Task.map((task) => {
+          if (task.completed) {
+            return { ...task, animationClass: 'animate__animated animate__fadeOutRightBig' }
+          }
+          return task
+        })
+        setTaskToDelete (completedTaskIds)
+        setTimeout (() => {
+            const NewTask = updatedTask.filter((task) => task.completed)
+            setTask(NewTask)
+        }, 500)
+    }
+
+    return ({
+        currentFilters,
+        createTask,
+        HasTask,
+        handleToggle,
+        handleDelete,
+        handleFilterChange,
+        filteredTask,
+        completedTask,
+        allTask,
+        handleDeleteAll,
+        TaskToDelete
+        }
+    )
 }
